467ff9c5619a58059a974ace8d0edd15870cc8be
Rafael Sergio <rafa.sergio.02@live.com>
2020-06-10 22:51:11 -0300

    first commit

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
+
+import '@shared/infra/typeorm';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(errors());
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+  console.error(err);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
